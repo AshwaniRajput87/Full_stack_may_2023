b9563d152ccf6a4a699640394ba9dc84b65a421f
Ashwani Rajput <ashwin.rajput87@gmail.com>
2023-06-29 11:01:53 +0530

    arrays explanations updated

--- Javascript/Lec_5_Array_HOFs_Pollyfills_of_HOFs/1_Array.js
+++ Javascript/Lec_5_Array_HOFs_Pollyfills_of_HOFs/1_Array.js
@@ -34,29 +34,37 @@ let arr1 = [1,2];
 console.log(arr1.constructor);// Array()
 
 /**
- *  Most used method of array: 
- *  1. push()
- *  2. pop()
- *  3. unshift()
- *  4. shift()
- *  5. isArray()
- *  6. reverse();
- *  7. sort()
- *  8. tostring()
- *  9. concat(): merging 2 or more array.
- *  10. indexOf()
- *  11. lastIndexOf()
- *  12. includes()
- *  13. slice(): Extract the protion of an array into a new array, 
- *      without modification the original array
+ *  Most used methods of array: 
+ *  1. push(): Adds one or more elements to the end of an array.
+ *  2. pop(): Removes the last element from an array and returns it.
+ *  3. unshift(): Adds one or more elements to the beginning of an array.
+ *  4. shift(): Removes the first element from an array and returns it.
+ *  5. isArray(): Checks if a value is an array or not and returns a boolean value(true/false).
+ *  6. reverse(): Reverses the order of the elements in an array.
+ *  7. sort(): Sorts the elements of an array in place. 
+ *  8. tostring():Converts an array to a string.
+ *  9. concat(): merging 2 or more arrays and returns a new array.
+ *  10. indexOf(): Returns the first index at which 
+ *      a specified element is found in an array.
+ *  11. lastIndexOf(): Returns the last index 
+ *      at which a specified element is found in an array.
+ *  12. includes(): Checks if an array contains a specified element and 
+ *      returns a boolean value.
+ *  13. join(): Joins all elements of an array into a string.
+ *  14. slice(): Extract the protion of an array into a new array, 
+ *      without modificying the original array.
  *      syntax: slice(start, end) end(exclusive)
- *  14. splice(): 
- * 
+ *      NOTE: It supports negative index also.
+ *      
+ *  15. splice(): Changes the contents of an array by removing, 
+ *      replacing, or adding elements, but it modifies the original array. 
+ *      syntax: array.splice(start, deleteCount, item1, item2, ..., itemN);
+ *      NOTE: It doesn't support negative index.
  * IQ: slice() vs splice()
  */ 
         //      0        1        2
 let fruits = ['apple', 'banana', 'orange'];
-               -3            -2       -1
+        //      -3            -2       -1
 let newFruitsBucket = fruits.slice();
 newFruitsBucket.push('kiwi')
 
